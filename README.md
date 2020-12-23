b1b1014ee3b7f5b054924708ca2b569334a95641
Michał Lampert <michlampert@pop-os.localdomain>
2020-12-23 07:13:51 +0100

    Add C-C and Loop implementation, README updated

--- README.md
+++ README.md
@@ -1,25 +1,36 @@
 # subdivision
-
-### [Michał Lampert](https://github.com/michlampert/) & [Wojciech Achtelik](https://github.com/WojtAcht/)
+## Simple implementation of three most common subdivision algorithms
+### by [Michał Lampert](https://github.com/michlampert/) & [Wojciech Achtelik](https://github.com/WojtAcht/)
 
 - [x] Doo–Sabin
-- [ ] Catmull–Clark
+- [x] Catmull–Clark
+- [x] Loop
 
-![suzanne](suzanne.gif)
-
-| stats | |
-| --- | --- |
-| mesh after 0 subdivisions<br />vertices: 8<br />faces: 6<br />time to compute: 0 ms | <img src="photos/photo00_L00.png" alt="drawing" width="300"/> |
-| mesh after 1 subdivisions<br />vertices: 24<br />faces: 26<br />time to compute: 0.75674 ms | <img src="photos/photo00_L01.png" alt="drawing" width="300"/> |
-| mesh after 2 subdivisions<br />vertices: 96<br />faces: 98<br />time to compute: 3.03 ms | <img src="photos/photo00_L02.png" alt="drawing" width="300"/> |
-| mesh after 3 subdivisions<br />vertices: 384<br />faces: 386<br />time to compute: 10.57 ms | <img src="photos/photo00_L03.png" alt="drawing" width="300"/> |
-| mesh after 4 subdivisions<br />vertices: 1536<br />faces: 1538<br />time to compute: 46.89 ms | <img src="photos/photo00_L04.png" alt="drawing" width="300"/> |
-| mesh after 5 subdivisions<br />vertices: 6144<br />faces: 6146<br />time to compute: 0.2 s | <img src="photos/photo00_L05.png" alt="drawing" width="300"/> |
-| mesh after 6 subdivisions<br />vertices: 24576<br />faces: 24578<br />time to compute: 1.25 s | <img src="photos/photo00_L06.png" alt="drawing" width="300"/> |
-| mesh after 7 subdivisions<br />vertices: 98304<br />faces: 98306<br />time to compute: 19.04 s | <img src="photos/photo00_L07.png" alt="drawing" width="300"/> |
+## 1. Step by step effect of Catmull-Clark algorithm
+![suzanne](suzanne_CC.gif)
 
 
-<!-- | file: dodecahedron<br />number of steps: 5<br />faces before: 20<br />faces after: 15362<br />time to compute: 0.74 s | file: icosahedron<br />number of steps: 5<br />faces before: 12<br />faces after: 15362<br />time to compute: 0.73 s | file: boxtorus<br />number of steps: 4<br />faces before: 32<br />faces after: 8192<br />time to compute: 0.27 s |
+## 2. Statistics and results for Suzanne (Blender monkey)
+below images: number of vertices | number of faces | time to compute
+| Doo-Sabin | Catmull-Clark | Loop |
 |---|---|---|
-| <img src="photos/photo01_L03.png" alt="drawing" width="50%"/> | <img src="photos/photo01_L05.png" alt="drawing" width="50%"/> | <img src="photos/photo01_L01.png" alt="drawing" width="50%"/> |
-|  <img src="photos/photo01_L02.png" alt="drawing" width="50%"/> | <img src="photos/photo01_L04.png" alt="drawing" width="50%"/> | <img src="photos/photo01_L00.png" alt="drawing" width="50%"/> | -->
+| ![img](photos/group_photo01_L06.png) <br/> 507 \| 500 \| 0 ms | ![img](photos/group_photo01_L00.png) <br/> 507 \| 500 \| 0 ms | ![img](photos/group_photo01_L12.png) <br/> 507 \| 968 \| 0 ms |
+| ![img](photos/group_photo01_L07.png) <br/> 1968 \| 1927 \| 27.16 ms | ![img](photos/group_photo01_L01.png) <br/> 2012 \| 1968 \| 37.93 ms | ![img](photos/group_photo01_L13.png) <br/> 1980 \| 3872 \| 0.13 s |
+| ![img](photos/group_photo01_L08.png) <br/> 7702 \| 7620 \| 0.15 s | ![img](photos/group_photo01_L02.png) <br/> 7958 \| 7872 \| 0.18 s | ![img](photos/group_photo01_L14.png) <br/> 7830 \| 15488 \| 0.47 s |
+| ![img](photos/group_photo01_L09.png) <br/> 30472 \| 30306 \| 1.12 s | ![img](photos/group_photo01_L03.png) <br/> 31658 \| 31488 \| 2.71 s | ![img](photos/group_photo01_L15.png) <br/> 31146 \| 61952 \| 1.98 s |
+| ![img](photos/group_photo01_L10.png) <br/> 121216 \| 120882 \| 27.22 s | ![img](photos/group_photo01_L04.png) <br/> 126290 \| 125952 \| 6.4 s | ![img](photos/group_photo01_L16.png) <br/> 124242 \| 247808 \| 9.78 s |
+| ![img](photos/group_photo01_L11.png) <br/> 483520 \| 482850 \| 638.77 s | ![img](photos/group_photo01_L05.png) <br/> 504482 \| 503808 \| 26.93 s | ![img](photos/group_photo01_L17.png) <br/> 496290 \| 991232 \| 34.1 s |
+|![suzanne](suzanne_DS.gif)|![suzanne](suzanne_CC.gif)|![suzanne](suzanne_LOOP.gif)|
+
+## 3. Statistics and results for cube
+below images: number of vertices | number of faces | time to compute
+| Doo-Sabin | Catmull-Clark | Loop |
+|---|---|---|
+| ![img](photos/group_photo00_L08.png) <br/> 8 \| 6 \| 0 ms | ![img](photos/group_photo00_L00.png) <br/> 8 \| 6 \| 0 ms | ![img](photos/group_photo00_L16.png) <br/> 8 \| 12 \| 0 ms |
+| ![img](photos/group_photo00_L09.png) <br/> 24 \| 26 \| 0.4127 ms | ![img](photos/group_photo00_L01.png) <br/> 26 \| 24 \| 0.65184 ms | ![img](photos/group_photo00_L17.png) <br/> 26 \| 48 \| 0.76866 ms |
+| ![img](photos/group_photo00_L10.png) <br/> 96 \| 98 \| 1.73 ms | ![img](photos/group_photo00_L02.png) <br/> 98 \| 96 \| 3.87 ms | ![img](photos/group_photo00_L18.png) <br/> 98 \| 192 \| 5.18 ms |
+| ![img](photos/group_photo00_L11.png) <br/> 384 \| 386 \| 8.28 ms | ![img](photos/group_photo00_L03.png) <br/> 386 \| 384 \| 11.22 ms | ![img](photos/group_photo00_L19.png) <br/> 386 \| 768 \| 15.26 ms |
+| ![img](photos/group_photo00_L12.png) <br/> 1536 \| 1538 \| 27.7 ms | ![img](photos/group_photo00_L04.png) <br/> 1538 \| 1536 \| 36.12 ms | ![img](photos/group_photo00_L20.png) <br/> 1538 \| 3072 \| 45.71 ms |
+| ![img](photos/group_photo00_L13.png) <br/> 6144 \| 6146 \| 0.11 s | ![img](photos/group_photo00_L05.png) <br/> 6146 \| 6144 \| 0.13 s | ![img](photos/group_photo00_L21.png) <br/> 6146 \| 12288 \| 0.85 s |
+| ![img](photos/group_photo00_L14.png) <br/> 24576 \| 24578 \| 0.73 s | ![img](photos/group_photo00_L06.png) <br/> 24578 \| 24576 \| 0.92 s | ![img](photos/group_photo00_L22.png) <br/> 24578 \| 49152 \| 2.0 s |
+| ![img](photos/group_photo00_L15.png) <br/> 98304 \| 98306 \| 16.61 s | ![img](photos/group_photo00_L07.png) <br/> 98306 \| 98304 \| 3.24 s | ![img](photos/group_photo00_L23.png) <br/> 98306 \| 196608 \| 5.52 s |
--- mesh.py
+++ mesh.py
@@ -4,15 +4,20 @@ import itertools
 import time
 import math
 
+global_counter = itertools.count(10000)
+
 class Vertex:
     def __init__(self, x, y, z, faces = None, id = None):
         self.x = x
         self.y = y
         self.z = z
         self.faces = faces if faces else []
-        self.id = id
+        self.id = id or next(global_counter)
 
-    def __str__(self): return f"{self.id}:({self.x},{self.y},{self.z})"
+    def copy(self, id=None):
+        return Vertex(self.x, self.y, self.z, [], id)
+
+    def __str__(self): return f"{self.id}:({round(self.x,2)},{round(self.y,2)},{round(self.z,2)})"
 
     def dist(self, other): return ((self.x - other.x)**2 + (self.y - other.y)**2 + (self.z - other.z)**2)**(1/2)
 
@@ -28,6 +33,9 @@ class Vertex:
     def __mul__(self, num):
         return Vertex(num * self.x, num * self.y, num * self.z)
 
+    def __truediv__(self, num):
+        return self * (1/num)
+
     def repair_faces_order(self):
         if self.faces == []: return
 
@@ -37,58 +45,85 @@ class Vertex:
         result = [faces.pop(0)]
 
         counter = itertools.count()
+
+        limit = len(self.faces)**2
+
         while faces:
             face = faces.pop(0)
-            if face in result[-1].neighbours():
+            if face in result[-1].get_neighbours():
                 result.append(face)
             else:
                 faces.append(face)
-                if next(counter) == 1000:
+                if next(counter) == limit:
                     result.reverse()
-                if next(counter) == 2000:
+                if next(counter) == 2 * limit:
                     print(*self.faces)
                     return
-                if next(counter) == 2000:
+                if next(counter) == 1000:
                     print(*self.faces)
-                    return
                     raise Exception("Bad structure detected in vertex:")
 
 
         self.faces = result
+
+    def get_neighbours(self):
+        result = set()
+
+        for face in self.faces:
+            idx = face.vertices.index(self)
+            result.add(face.vertices[idx - 1])
+            result.add(face.vertices[(idx + 1) % len(face.vertices)])
+
+        return list(result)
+
+    def __hash__(self) -> int:
+        return self.id
 
 class Face:
     def __init__(self, vertices = None):
         self.vertices = vertices if vertices else []
         for v in self.vertices:
             v.faces.append(self)
-        if self.vertices: self.center = Vertex(sum([v.x for v in self.vertices]), sum([v.y for v in self.vertices]), sum([v.z for v in self.vertices])) * (1/len(self.vertices))
+        if self.vertices:
+            self.center = Vertex(sum([v.x for v in self.vertices]), sum([v.y for v in self.vertices]), sum([v.z for v in self.vertices])) * (1/len(self.vertices))
         self.inside_points = {}
+        self.neighbours = []
 
     def __str__(self): return "[" + " ".join(list(map(lambda v: str(v.id), self.vertices))) + "]"
 
-    def neighbours(self):
-        result = []
-        for v1, v2 in zip(self.vertices, self.vertices[1:] + self.vertices[:1]):
-            tmp = set().union(v1.faces).intersection(v2.faces).difference([self])
-            if tmp != set():
-                result.append(tmp.pop())
+    def get_neighbours(self):
+        if self.neighbours != []:
+            return self.neighbours
+        else:
+            result = []
+            for v1, v2 in zip(self.vertices, self.vertices[1:] + self.vertices[:1]):
+                tmp = set().union(v1.faces).intersection(v2.faces).difference([self])
+                if tmp != set():
+                    result.append(tmp.pop())
 
-        return result
+            self.neighbours = result
+            return result
 
-    def get_inside_points(self):
+    def get_inside_points(self, ratio = 0.5):
         if self.inside_points != {}:
             return self.inside_points
         else:
-            result = {v: (v + self.center) * 0.5 for v in self.vertices}
+            result = {v: (v * ratio + self.center * (1 - ratio)) for v in self.vertices}
             self.inside_points = result
             return result
 
+    def get_neighbour(self, v1, v2):
+        for neighbour in self.get_neighbours():
+            if v1 in neighbour.vertices and v2 in neighbour.vertices:
+                return neighbour
+        return None
+
 class Mesh:
-    def __init__(self, *, vertices = None, faces = None, filename = None):
+    def __init__(self, *, vertices = None, faces = None, filename = None, triangular=False):
         if vertices is None and faces is None:
             self.vertices = []
             self.faces = []
-        if vertices and faces:
+        if vertices or faces:
             self.vertices = vertices
             self.faces = faces
         if filename is not None:
@@ -123,16 +158,23 @@ class Mesh:
             for i in range(faces_count):
                 line = lines.pop(0)
                 c, *vs = [int(num) for num in line.strip().split(" ") if num]
-                
-                f = Face(list(map(lambda num: vertices[num], vs)))
-                self.faces.append(f)
+                if c != 3 and triangular:
+                    vs = list(map(lambda num: vertices[num], vs))
+                    main = vs.pop(0)
+                    for v1, v2 in zip(vs, vs[1:]):
+                        self.faces.append(Face([main, v1, v2]))
+                else:
+                    self.faces.append(Face(list(map(lambda num: vertices[num], vs))))
                 # for v in vs: vertices[v].faces.append(f)
 
             self.vertices += vertices.values()
+        
+        # self.vertices = list(filter(lambda v: len(v.faces) > 0, self.vertices))
+        # self.faces = list(filter(lambda f: len(f.vertices) > 0, self.faces))
 
 
     def __str__(self):
-        return f"vertices: {list(map(str, self.vertices))}\nfaces: {list(map(lambda f: list(map(lambda v: v.id, f.vertices)), self.faces))}"
+        return f"vertices:\n{list(map(str, self.vertices))}\nfaces:\n{list(map(lambda f: list(map(lambda v: v.id, f.vertices)), self.faces))}"
 
     def save(self, filename):
         file = open(filename, "w+")
@@ -146,8 +188,12 @@ class Mesh:
         for f in self.faces:
             print(len(f.vertices), *list(map(lambda v: mapping[v.id], f.vertices)), file=file)
 
-    def subdivision(self):
-        new_vertices = sum([list(face.get_inside_points().values()) for face in self.faces],[])
+    def save_faces_separately(self):
+        for i, face in enumerate(self.faces):
+            Mesh(vertices=face.vertices, faces=[face]).save(f"tmp/face{i}.off")
+
+    def subdivision_DS(self, ratio = 0.5):
+        new_vertices = sum([list(face.get_inside_points(ratio).values()) for face in self.faces],[])
         counter = itertools.count()
         for v in new_vertices: v.id = next(counter)
         
@@ -180,11 +226,105 @@ class Mesh:
 
         return Mesh(vertices=new_vertices, faces=new_faces)
 
-    def save_faces_separately(self):
-        for i, face in enumerate(self.faces):
-            Mesh(vertices=self.vertices, faces=[face]).save(f"tmp/face{i}.off")
+    def subdivision_CC(self):
 
+        edge_points = {}
+        vertex_points = {}
 
+        for face in self.faces:
+            for v1, v2 in zip(face.vertices, face.vertices[1:] + face.vertices[:1]):
+                if (v1, v2) in edge_points.keys() or (v2, v1) in edge_points.keys(): continue
+                neighbour = face.get_neighbour(v1,v2) #or face.get_neighbour(v2,v1)
+                if neighbour:
+                    v = (v1 + v2 + face.center + neighbour.center)/4
+                    edge_points[(v1,v2)] = v  
+                else:
+                    v = (v1 + v2 + face.center)/3
+                    edge_points[(v1,v2)] = v 
+
+        for v in self.vertices:
+            # esc = Face([(edge_points.get((v1,v), None) or edge_points.get((v,v1), None) or print(v, v1)) for v1 in v.get_neighbours()]).center
+            # fsc = Face([face.center for face in v.faces]).center
+            esc = sum([(v + v1) / 2 for v1 in v.get_neighbours()], Vertex(0,0,0)) / (len(v.get_neighbours()))
+            fsc = sum([face.center for face in v.faces], Vertex(0,0,0)) / (len(v.faces))
+            n = len(v.faces)#?
+            vertex_points[v] = ((v * (n-3)) + (esc * 2) + fsc) / n
+            # print(n, "    ", esc, "    ", fsc, "    ", ((v * (n-3)) + (esc * 2) + fsc) / n)
+
+        new_faces = []
+        new_vertices = set()
+
+        for face in self.faces:
+            fc = face.center.copy()
+            new_vertices.add(fc)
+            for v1, v2, v3 in zip(face.vertices, face.vertices[1:] + face.vertices[:1], face.vertices[2:] + face.vertices[:2]):
+                ec1 = (edge_points.get((v1,v2), None) or edge_points.get((v2,v1), None))
+                ec2 = (edge_points.get((v2,v3), None) or edge_points.get((v3,v2), None))
+                v = vertex_points[v2]
+                new_faces.append(Face([ec1, v, ec2, fc]))
+
+        new_vertices.update(edge_points.values())
+        new_vertices.update(vertex_points.values())
+        
+        new_vertices = list(new_vertices)
+
+        for i, v in enumerate(new_vertices): v.id = i
+
+        new_vertices = sorted(new_vertices, key=lambda v: v.x)
+        return Mesh(vertices=new_vertices, faces=new_faces)
+
+    def subdivision_LOOP(self):
+
+        def alphas(n):
+            if alphas.d.get(n, None): return alphas.d.get(n)
+            else:
+                a = 3/8 + (3/8 + math.cos(2*math.pi / n)/4)**2
+                alphas.d[n]=a
+                return a
+        alphas.d = {}
+
+        edge_points = {}
+        vertex_points = {}
+
+        for face in self.faces:
+            for v1, v2 in zip(face.vertices, face.vertices[1:] + face.vertices[:1]):
+                if (v1, v2) in edge_points.keys() or (v2, v1) in edge_points.keys(): continue
+                neighbour = face.get_neighbour(v1,v2) #or face.get_neighbour(v2,v1)
+                if neighbour:
+                    v = ((v1 + v2)*(1/8) + (face.center + neighbour.center)*(3/8))
+                    edge_points[(v1,v2)] = v  
+                else:
+                    v = (v1 + v2)/2
+                    edge_points[(v1,v2)] = v 
+
+        for v in self.vertices:
+            n = len(v.get_neighbours())
+            esc = sum([v1 for v1 in v.get_neighbours()], Vertex(0,0,0)) / n
+            vertex_points[v] = v*alphas(n) + esc * (1-alphas(n))
+            # print(n, "    ", esc, "    ", fsc, "    ", ((v * (n-3)) + (esc * 2) + fsc) / n)
+
+        new_faces = []
+        new_vertices = set()
+
+        for face in self.faces:
+            new_faces.append(Face([edge_points.get((v1,v2), None) or edge_points.get((v2,v1), None) for v1,v2 in zip(face.vertices, face.vertices[1:] + face.vertices[:1])]))
+            for v1, v2, v3 in zip(face.vertices, face.vertices[1:] + face.vertices[:1], face.vertices[2:] + face.vertices[:2]):
+                ec1 = (edge_points.get((v1,v2), None) or edge_points.get((v2,v1), None))
+                ec2 = (edge_points.get((v2,v3), None) or edge_points.get((v3,v2), None))
+                v = vertex_points[v2]
+                new_faces.append(Face([ec1, v, ec2]))
+
+        new_vertices.update(edge_points.values())
+        new_vertices.update(vertex_points.values())
+        
+        new_vertices = list(new_vertices)
+
+        for i, v in enumerate(new_vertices): v.id = i
+
+        new_vertices = sorted(new_vertices, key=lambda v: v.x)
+        return Mesh(vertices=new_vertices, faces=new_faces)
+
+        
 
 if __name__ == "__main__":
 
@@ -199,53 +339,98 @@ if __name__ == "__main__":
     #     elif total_time < 0.1:
     #         time_str = str(round(total_time * 1000, 2)) + " ms"
     #     else: time_str = str(round(total_time, 2)) + " s"
-    #     print(f'| mesh after {i} subdivisions<br />vertices: {len(mesh.vertices)}<br />faces: {len(mesh.faces)}<br />time to compute: {time_str} | <img src="photos/photo00_L0{i}.png" alt="drawing" width="50%"/> |')
+    #     print(f'| mesh after {i} subdivision_DSs<br />vertices: {len(mesh.vertices)}<br />faces: {len(mesh.faces)}<br />time to compute: {time_str} | <img src="photos/photo00_L0{i}.png" alt="drawing" width="50%"/> |')
         
     #     if i == 7 : break
     #     t1 = time.time()
-    #     mesh = mesh.subdivision()
+    #     mesh = mesh.subdivision_DS()
     #     t2 = time.time()
     #     total_time += t2-t1
 
-    # for filename in ["boxtorus","dodecahedron","icosahedron"]:
-    #     mesh = Mesh(filename=f"meshes/{filename}.off")
-    #     sth = len(mesh.vertices)
-    #     t1 = time.time()
-    #     for i in range(6 - int(- 0.5 + math.log(len(mesh.vertices), 4))):
-    #         mesh = mesh.subdivision()
-    #     total_time = time.time() - t1
-    #     if total_time < 0.001:
-    #         time_str = str(round(total_time * 1000, 5)) + " ms"
-    #     elif total_time < 0.1:
-    #         time_str = str(round(total_time * 1000, 2)) + " ms"
-    #     else: time_str = str(round(total_time, 2)) + " s"
-    #     mesh.save(f"meshes/{filename}_smooth.off")
-        # print(f'| file: {filename}<br />number of steps: {i+1}<br />faces before: {sth}<br />faces after: {len(mesh.faces)}<br />time to compute: {time_str} | <img src="photos/photo00_L0{2*i}.png" alt="drawing" width="50%"/> | <img src="photos/photo00_L0{2*i + 1}.png" alt="drawing" width="50%"/> |')
-
-    # mesh = Mesh(filename = "meshes/heart.off")
-    # mesh.subdivision().save("meshes/heart_smooth.off")
-
-    # for filename in "boxtorus dodecahedron icosahedron m719 m724 m725 m747 m727 m746 m739 m757 m786 m804 m1600 m1813".split(" "):
-    #     mesh = Mesh(filename=f"best_meshes/{filename}.off")
-    #     # sth = len(mesh.vertices)
-    #     # t1 = time.time()
-    #     try:
-    #         for i in range(6 - int(- 0.5 + math.log(len(mesh.vertices), 4))):
-    #             mesh = mesh.subdivision()
-    #         mesh.save(f"best_meshes/{filename}_smooth.off")
-    #     except:
-    #         print(filename+".off", end=" ")
-
-        # total_time = time.time() - t1
-        # if total_time < 0.001:
-        #     time_str = str(round(total_time * 1000, 5)) + " ms"
-        # elif total_time < 0.1:
-        #     time_str = str(round(total_time * 1000, 2)) + " ms"
-        # else: time_str = str(round(total_time, 2)) + " s"
-        # print(f'| file: {filename}<br />number of steps: {i+1}<br />faces before: {sth}<br />faces after: {len(mesh.faces)}<br />time to compute: {time_str} | <img src="photos/photo00_L0{2*i}.png" alt="drawing" width="50%"/> | <img src="photos/photo00_L0{2*i + 1}.png" alt="drawing" width="50%"/> |')
     
-    mesh = Mesh(filename="suzanne.off")
-    for i in range(5):
-        mesh = mesh.subdivision()
-        mesh.save(f"suzanne_smooth_{i+1}.off")
-    
+    # mesh = Mesh(filename="best_meshes/suzanne.off")
+    # for i in range(3):
+    #     mesh = mesh.subdivision_LOOP()
+    #     mesh.save(f"suzanne_smooth_{i+1}.off")
+
+    # mesh = Mesh(filename="cube.off")
+    # mesh.subdivision_CC().save("cube1.off")
+    # print("\n\n\n\n\n\n")
+    # mesh = Mesh(filename="cube1.off")
+    # mesh.subdivision_CC().save("cube2.off")
+    # print("\n\n\n\n\n\n")
+    # mesh = Mesh(filename="cube2.off")
+    # mesh.subdivision_CC().save("cube3.off")
+    # mesh.subdivision_CC().subdivision_CC().save("cube2.off")
+    # mesh.subdivision_CC().subdivision_CC().subdivision_CC().save("cube3.off")
+    # mesh.subdivision_CC().subdivision_CC().subdivision_CC().subdivision_CC().save("cube4.off")
+
+    # mesh = Mesh(filename = "cube.off")
+    # mesh.save("cube1.off")
+    # mesh.subdivision_LOOP().save("cube2.off")
+    # mesh.subdivision_LOOP().subdivision_LOOP().save("cube3.off")
+    # mesh.subdivision_LOOP().subdivision_LOOP().subdivision_LOOP().save("cube4.off")
+
+    mesh_DS = Mesh(filename = "suzanne.off")
+    mesh_CC = Mesh(filename = "suzanne.off")
+    mesh_LOOP = Mesh(filename = "suzanne.off", triangular=True)
+
+    DS_list = []
+    CC_list = []
+    LOOP_list = []
+
+    n = 6
+
+    total_time = 0
+    for i in range(n):
+        mesh_DS.save(f"cube_DS{i}.off")
+
+        if total_time < 0.001:
+            time_str = str(round(total_time * 1000, 5)) + " ms"
+        elif total_time < 0.1:
+            time_str = str(round(total_time * 1000, 2)) + " ms"
+        else: time_str = str(round(total_time, 2)) + " s"
+        DS_list.append(f'![img](photos/group_photo01_L{(str(i+n) if i+n>=10 else "0"+str(i+n))}.png) <br/> {len(mesh_DS.vertices)} \| {len(mesh_DS.faces)} \| {time_str}')
+        
+        if i == n-1 : break
+        t1 = time.time()
+        mesh_DS = mesh_DS.subdivision_DS()
+        t2 = time.time()
+        total_time += t2-t1
+
+    total_time = 0
+    for i in range(n):
+        mesh_CC.save(f"cube_CC{i}.off")
+
+        if total_time < 0.001:
+            time_str = str(round(total_time * 1000, 5)) + " ms"
+        elif total_time < 0.1:
+            time_str = str(round(total_time * 1000, 2)) + " ms"
+        else: time_str = str(round(total_time, 2)) + " s"
+        CC_list.append(f'![img](photos/group_photo01_L{(str(i) if i>=10 else "0"+str(i))}.png) <br/> {len(mesh_CC.vertices)} \| {len(mesh_CC.faces)} \| {time_str}')
+        
+        if i == n-1 : break
+        t1 = time.time()
+        mesh_CC = mesh_CC.subdivision_CC()
+        t2 = time.time()
+        total_time += t2-t1
+
+    total_time = 0
+    for i in range(n):
+        mesh_LOOP.save(f"cube_LOOP{i}.off")
+
+        if total_time < 0.001:
+            time_str = str(round(total_time * 1000, 5)) + " ms"
+        elif total_time < 0.1:
+            time_str = str(round(total_time * 1000, 2)) + " ms"
+        else: time_str = str(round(total_time, 2)) + " s"
+        LOOP_list.append(f'![img](photos/group_photo01_L{(str(i+n+n) if i+n+n>=10 else "0"+str(i+n+n))}.png) <br/> {len(mesh_LOOP.vertices)} \| {len(mesh_LOOP.faces)} \| {time_str}')
+        
+        if i == n-1 : break
+        t1 = time.time()
+        mesh_LOOP = mesh_LOOP.subdivision_LOOP()
+        t2 = time.time()
+        total_time += t2-t1
+
+    for e1, e2, e3 in zip(DS_list, CC_list, LOOP_list):
+        print("|",e1,"|",e2,"|",e3,"|")
